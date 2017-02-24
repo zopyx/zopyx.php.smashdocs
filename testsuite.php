b940aeec9ae47f535fa705b38c32f02d2b598f93
Andreas Jung <info@zopyx.com>
2017-02-24 12:09:12 +0100

    more tests

--- testsuite.php
+++ testsuite.php
@@ -14,6 +14,12 @@ final class SmashdocTests extends TestCase
         parent::__construct();
     }
 
+    function _createDocument() {
+
+        $result = $this->sd->new_document();
+        return $result['documentId'];
+    }
+
     function testListTemplates()
     {
         $result = $this->sd->list_templates();
@@ -21,19 +27,34 @@ final class SmashdocTests extends TestCase
 
     function testCreateAndDeleteDocument()
     {
-        $result = $this->sd->new_document();
-        $documentId = $result['documentId'];
+        $documentId = $this->_createDocument();
+        $this->sd->delete_document($documentId);
+    }
+
+    function testArchiveUnarchive()
+    {
+        $documentId = $this->_createDocument();
+        $this->sd->archive_document($documentId);
+        $this->sd->unarchive_document($documentId);
         $this->sd->delete_document($documentId);
     }
 
     function testCreateAndDeleteTwice()
     {
-        $result = $this->sd->new_document();
-        $documentId = $result['documentId'];
+        $documentId = $this->_createDocument();
         $this->sd->delete_document($documentId);
         try {
             $this->sd->delete_document($documentId);
         } catch(DeletionError $e) { }
     }
+
+    function testOpenDocument()
+    {
+        $documentId = $this->_createDocument();
+        $result = $this->sd->open_document($documentId);
+        $this->assertContains('https://', $result['documentAccessLink']); 
+        $this->sd->delete_document($result['documentId']);
+    }
+
 }
 
